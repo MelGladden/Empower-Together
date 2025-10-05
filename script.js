81770adca4c2b277320db8d9590627094dfa2af5
MelGladden <Melissa.Gladden@my.columbiasc.edu>
2025-10-04 21:03:43 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("header");
+  const greeting = document.createElement("p");
+
+  const hour = new Date().getHours();
+  let message = "Welcome to Empower Together!";
+
+  if (hour < 12) {
+    message = "Good morning! " + message;
+  } else if (hour < 18) {
+    message = "Good afternoon! " + message;
+  } else {
+    message = "Good evening! " + message;
+  }
+
+  greeting.textContent = message;
+  greeting.style.fontStyle = "italic";
+  greeting.style.textAlign = "center";
+  header.appendChild(greeting);
+});
